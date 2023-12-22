53a01ee7a30be298588a3c140d45f76e0a32918e
Hrishi Punde <hrishipunde9882@gmail.com>
2023-12-22 13:15:55 +0530

    Products in Public class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -15,7 +15,7 @@ public class Main {
         System.out.println(ans1+" "+ans2+" "+ans3);
     }
 
-    static class Product {
+   public static class Product {
 
         public int product(int x, int y) {
             return x+y;
